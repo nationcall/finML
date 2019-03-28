6d9dd6915bae0e7b71ce31597ff855a5e8efdfbe
steve <sobbh00@gmail.com>
2019-03-28 22:50:45 +0800

    enable count by week

--- bars.py
+++ bars.py
@@ -1,5 +1,6 @@
 # define functions to generate bars
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def tick_bars(raw_data, num_ticks=1000):
@@ -26,23 +27,36 @@ def volume_bars(raw_data, num_vol=1000):
 
 
 def dollar_bars(raw_data, num_dollar=1e6):
-    dol_cum = np.cumsum(raw_data['size']*raw_data['price'])//num_dollar
+    dol_cum = np.cumsum(raw_data['dollar_vol'])//num_dollar
     diff = np.diff(dol_cum, 1)
-    diff = np.insert(diff,0,0)
+    diff = np.insert(diff, 0, 0)
     bool_diff = (diff == 1)
     dbs = raw_data.loc[bool_diff]
     print(dbs.head())
     return dbs
 
-# 2.1(b)
-def count_bars_per_week(bars):
-    wk_num = 
+
+def count_bars_per_week(bars): #count number of bars by week
+    count_week = bars.assign(count=1)
+    wk_num = count_week['count'].resample('W').sum()
+    return wk_num
+
 
 import pandas as pd
 
 raw_file = "IVE_tickbidask.txt"
 cols = ['date','time','price','bid','ask','size']
-raw_data = (pd.read_csv(raw_file,nrows=5000,header=None)
-            .rename(columns=dict(zip(range(len(cols)),cols))))
-#raw_data.head()
+raw_data = (pd.read_csv(raw_file, nrows=50000, header=None)
+            .rename(columns=dict(zip(range(len(cols)), cols)))
+            .assign(dates=lambda df:pd.to_datetime(df['date']+df['time'], format='%m/%d/%Y%H:%M:%S'))
+            .assign(dollar_vol=lambda df:df['size']*df['price'])
+            .drop(['date', 'time'], axis=1)
+            .set_index('dates')
+            .drop_duplicates())
+#print(raw_data.head())
 tbs, vbs, dbs = tick_bars(raw_data), volume_bars(raw_data), dollar_bars(raw_data)
+#count_bars_per_week(raw_data)
+
+# 2.1(b)
+tbs_wk, vbs_wk, dbs_wk = count_bars_per_week(tbs), count_bars_per_week(vbs), count_bars_per_week(dbs)
+plt.plot(tbs_wk)
